12f2f41b786f1db8cbc3b5a407b2b16276574230
sugayu <sugayu@aoni.waseda.jp>
2026-04-27 10:35:00 +0900

    Version up to v0.1.0

--- src/fighnd/__init__.py
+++ src/fighnd/__init__.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from . import config
 from logging import getLogger
 
-__version__ = '0.0.0'
+__version__ = '0.1.0'
 
 logger = getLogger(__name__)
 logger.info(f'HOMEPATH: {config.homepath}')
